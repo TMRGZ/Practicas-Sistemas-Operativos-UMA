9d52145fd84dd42ea8152b767c4a758714a62efe
MiguelRG <MiguelRG5605@Outlook.es>
2018-05-11 20:04:18 +0200

    Tarea 2: No toma bien el terminal

--- Shell_project.c
+++ Shell_project.c
@@ -33,39 +33,51 @@ int selectComandoInterno(char **cmd) {
 
 
 int main(void) {
-    char inputBuffer[MAX_LINE]; /* buffer to hold the command entered */
-    int background;             /* equals 1 if a command is followed by '&' */
-    char *args[MAX_LINE / 2];     /* command line (of 256) has max of 128 arguments */
+    char inputBuffer[MAX_LINE];     /* buffer to hold the command entered */
+    int background;                 /* equals 1 if a command is followed by '&' */
+    char *args[MAX_LINE / 2];       /* command line (of 256) has max of 128 arguments */
     // probably useful variables:
-    int pid_fork, pid_wait;    /* pid for created and waited process */
-    int status;                /* status returned by wait */
-    enum status status_res;    /* status processed by analyze_status() */
-    int info;                    /* info processed by analyze_status() */
+    int pid_fork, pid_wait;         /* pid for created and waited process */
+    int status;                     /* status returned by wait */
+    enum status status_res;         /* status processed by analyze_status() */
+    int info;                       /* info processed by analyze_status() */
 
-    while (1)                /* Program terminates normally inside get_command() after ^D is typed*/
+    while (1)                       /* Program terminates normally inside get_command() after ^D is typed*/
     {
         printf("COMMAND->");
         fflush(stdout);
+        ignore_terminal_signals();
         get_command(inputBuffer, MAX_LINE, args, &background);  /* get next command */
 
-        if (args[0] == NULL) continue;   // if empty command
+        if (args[0] == NULL) continue;          // if empty command
 
-        pid_fork = fork();
+        if (!selectComandoInterno(args)) {       // Comando interno?
+            pid_fork = fork();                  // Nuevo Proceso
 
-        if (pid_fork == 0) {
-            if (!selectComandoInterno(args)) {
+            if (pid_fork == 0) {                // Proceso Hijo
+                new_process_group(pid_fork);
+
+
+                if (!background) {               // Segundo Plano?
+                    restore_terminal_signals();
+                    set_terminal(pid_fork);
+
+                }
+
                 execvp(inputBuffer, args);
                 printf("Error, command not found: %s \n", args[0]);
                 exit(-1);
-            }
-        } else {
-            if (background) {
-                printf("Background job running... pid: %d, command: %s \n", getpid(), args[0]);
-            } else {
-                pid_wait = waitpid(pid_fork, &status, 0);
-                status_res = analyze_status(status, &info);
-                printf("Foreground pid: %d, command: %s, %s, info; %d \n", getpid(), args[0],
-                       status_strings[status_res], info);
+
+            } else {                            // Proceso Padre
+                if (background) {
+                    printf("Background job running... pid: %d, command: %s \n", pid_fork, args[0]);
+                } else {
+                    pid_wait = waitpid(pid_fork, &status, 0);
+                    set_terminal(getpid());
+                    status_res = analyze_status(status, &info);
+                    printf("Foreground pid: %d, command: %s, %s, info; %d \n", pid_wait, args[0],
+                           status_strings[status_res], info);
+                }
             }
         }
     } // end while
